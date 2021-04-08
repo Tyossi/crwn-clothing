430d9682725e3dfc94e24f515029f9263910ff5c
Yoshidda <70910101+Yoshidda@users.noreply.github.com>
2021-04-08 22:46:57 +0100

    updated app with Redirect and made tweaks to redux actions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import { connect } from "react-redux";
 import Header from "./components/header/header.component";
@@ -40,7 +40,17 @@ class App extends React.Component {
         <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route path="/signin" component={SignInAndSignUpPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to="/" />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
           <Route path="/shop" component={ShopPage} />
         </Switch>
       </div>
@@ -48,8 +58,12 @@ class App extends React.Component {
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/signin/signin.component.jsx
+++ src/components/signin/signin.component.jsx
@@ -1,60 +1,71 @@
-import React from 'react';
-import CustomButton from '../custom-button.component/custom-button.component';
-import FormInput from '../form-input/form-input.component';
-import './signin.styles.scss';
-import { signInWithGoogle } from "../../firebase/firebase.utils"
+import React from "react";
+import CustomButton from "../custom-button.component/custom-button.component";
+import FormInput from "../form-input/form-input.component";
+import "./signin.styles.scss";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 class SignIn extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            email: "",
-            password: ""
-        }
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    handleInputChange = (event) => {
-        const { name, value } = event.target
-        this.setState({ [name]: value })
-    }
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
 
-    handleSubmit = (event) => {
-        event.preventDefault();
-        this.setState({ email: "", password: "" })
-    }
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            handleInputChange={this.handleInputChange}
+            name="email"
+            type="email"
+            value={this.state.email}
+            label="email"
+            required
+          />
+          <FormInput
+            handleInputChange={this.handleInputChange}
+            name="password"
+            type="password"
+            value={this.state.password}
+            label="password"
+            required
+          />
+          <div className="buttons">
+            <CustomButton type="submit">Sign In</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              {" "}
+              Google Sign In{" "}
+            </CustomButton>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
 
-    render() {
-        return (
-            <div className="sign-in">
-                <h2>
-                    I already have an account
-                </h2>
-                <span>
-                    Sign in with your email and password
-                </span>
-
-                <form onSubmit={this.handleSubmit}>
-
-                    <FormInput handleInputChange={this.handleInputChange}
-                        name="email"
-                        type="email"
-                        value={this.state.email}
-                        label="email"
-                        required />
-                    <FormInput handleInputChange={this.handleInputChange}
-                        name="password"
-                        type="password"
-                        value={this.state.password}
-                        label="password"
-                        required />
-                    <div className="buttons">
-                        <CustomButton type="submit">Sign In</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>{' '}Google Sign In {' '}</CustomButton>
-                    </div>
-                </form>
-            </div>
-        )
-    }
-};
-
-export default SignIn
+export default SignIn;
