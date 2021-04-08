a191110c030e89da874dd6035c78ec30721348a2
Yoshidda <70910101+Yoshidda@users.noreply.github.com>
2021-04-08 20:49:23 +0100

    added redux and implemented user reducer and user action to header and app components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,34 +1,39 @@
-import React from 'react';
+import React from "react";
 import "./header.component.styles.scss";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { ReactComponent as Logo } from "../../assets/crown.svg"
-import { auth } from "../../firebase/firebase.utils"
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { auth } from "../../firebase/firebase.utils";
 
 const Header = ({ currentUser }) => {
-    return (
-        <div className="header">
-            <Link className="logo-container" to="/">
-                <Logo className="logo" />
-            </Link>
-            <div className="options">
-                <Link className="option" to="/shop">
-                    SHOP
-                </Link>
-                <Link className="option" to="/contact">
-                    CONTACT
-                </Link>
-                {
-                    currentUser ?
-                        (<div onClick={() => auth.signOut()} className="options">
-                            SIGN OUT
-                        </div>) :
-                        <Link to="/signin" className="option">
-                            SIGN IN
-                         </Link>
-                }
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <Link className="logo-container" to="/">
+        <Logo className="logo" />
+      </Link>
+      <div className="options">
+        <Link className="option" to="/shop">
+          SHOP
+        </Link>
+        <Link className="option" to="/contact">
+          CONTACT
+        </Link>
+        {currentUser ? (
+          <div onClick={() => auth.signOut()} className="options">
+            SIGN OUT
+          </div>
+        ) : (
+          <Link to="/signin" className="option">
+            SIGN IN
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default Header;
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+});
+
+export default connect(mapStateToProps)(Header);
